9f32c76843af8ae2770e447b0b634321a553296c
Jake Harbarenko <Jake@jakes-macbook-pro.local>
2016-05-20 10:43:16 -0400

    New Design GUI

--- Deez nuts/AppDelegate.swift
+++ Deez nuts/AppDelegate.swift
@@ -11,10 +11,11 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+   
 
+    
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+       func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
 
